2989159ec3023e2a4495a21a256b4c079f8b8a21
Irakli Gozalishvili <rfobic@gmail.com>
2010-10-26 04:13:22 +0200

    Adding more tests to the collection.

--- test/all.js
+++ test/all.js
@@ -1,5 +1,7 @@
 'use strict'
 
 exports['test `all`'] = require('./promised-all')
+exports['test `Callback`'] = require('./promised-callback')
+exports['test `Promised`'] = require('./promised-function')
 
 if (module == require.main) require('test').run(exports)
